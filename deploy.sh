130357d8f1712386a525354c5acab23422b5cca7
hazeluff <me@hazeluff.com>
2016-11-03 22:16:10 -0700

    Update deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 # Run this to deploy a new jar to the server and restart the service
 # 1) /etc/init.d/canucksbot should be setup on server
-# 2) canucksbot user should be created on server
-# 3) ssh keys need to be created    
-#    canucksbot user should have public ssh key in ~/.ssh/authorized_keys
-#    canucksbot private key should be at ~/.ssh/canucksbot
-#    sudo visudo and add 'canucksbot ALL=NOPASSWD: /usr/bin/service canucksbot *' to bottom of the list
+# 2) Your user needs sudo access, and run 'sudo service' with no password
+#    sudo visudo and add 'user ALL=NOPASSWD: /usr/bin/service canucksbot *' to bottom of the list
+#
+# HOW TO RUN:
 # deploy.sh [versionNumber] [serverName]
-scp -i ~/.ssh/canucksbot target/canucksbot-$1-jar-with-dependencies.jar canucksbot@$2:~/builds
-COMMANDS="mv builds/canucksbot-$1-jar-with-dependencies.jar builds/canucksbot-$1.jar && sudo /usr/bin/service canucksbot stop && ln -sfn builds/canucksbot-$1.jar canucksbot && sudo /usr/bin/service canucksbot start"
-echo $COMMANDS
-ssh -i ~/.ssh/canucksbot canucksbot@$2 $COMMANDS
+scp target/canucksbot-$1-jar-with-dependencies.jar $2:~
+COMMANDS="cd /opt/canucksbot && sudo /bin/mv ~/canucksbot-$1-jar-with-dependencies.jar /opt/canucksbot/builds/canucksbot-$1.jar && sudo /usr/bin/service canucksbot stop && ln -sfn builds/canucksbot-$1.jar canucksbot && sudo /usr/bin/service canucksbot start"
+echo "Executing commands on server: $COMMANDS"
+ssh $2 $COMMANDS
