ccc434870e7584a6c5b3475e9c3632eebc5dcdaa
hazeluff <me@hazeluff.com>
2017-01-10 20:08:00 -0800

    Update custom messages

--- src/main/java/com/hazeluff/discord/nhlbot/nhl/custommessages/CanucksCustomMessages.java
+++ src/main/java/com/hazeluff/discord/nhlbot/nhl/custommessages/CanucksCustomMessages.java
@@ -14,23 +14,23 @@ public class CanucksCustomMessages {
 	static {
 		aliases.put(8467876, "GOAL! **Hanky**!");
 		aliases.put(8467875,
-				"Goal for **Danny**. With extra peanut butter. :peanuts:");
+				":peanuts: Lowlife goal! :peanuts:");
 		aliases.put(8470626, "**LUUUUUUUUUU**");
-		aliases.put(8471303, ":eagle: **Eagle** Scores :eagle:");
-		aliases.put(8471498, "Goal! The **Honey Badger** is hungry.");
-		aliases.put(8474579, "**Luca**'s Pizza :pizza: Delivery's here!");
+		aliases.put(8471303, ":eagle:");
+		aliases.put(8471498, "**Honey Badger**!");
+		aliases.put(8474579, ":pizza: delivery!");
 		aliases.put(8475690, "Our lord and saviour, **ChrisT**, has arrived!");
 		aliases.put(8475790, "Real **Gud** goal!");
-		aliases.put(8476466, "Thx **Bae** <3.");
-		aliases.put(8477018, ":smile: Goal! **Ben Hutton** :smile:");
-		aliases.put(8477500, "They don't call him **Bo Scorevat** for no reason.");
-		aliases.put(8477937, "BIG COUNTRY GOAL :cowboy:");
-		aliases.put(8473415, "Goal by **Bulldog**.");
+		aliases.put(8476466, ":bear: :cheese:");
+		aliases.put(8477018, ":smile:");
+		aliases.put(8477500, "#:hotdog:Horvat");
+		aliases.put(8477937, "BIG COUNTRY :cowboy:");
+		aliases.put(8473415, "**Bulldog**!");
 
 		// Ex-Players
 		aliases.put(8470616, "Fuck! Goal by **Guzzler**");
 		aliases.put(8469598, "Not like this... We miss you **Juice** :(");
-		aliases.put(8469465, "**Hammer** time! :hammer:");
+		aliases.put(8469465, ":hammer: time!");
 
 	}
 
@@ -46,7 +46,7 @@ public class CanucksCustomMessages {
 		if (players.size() > 1) {
 			if (players.get(0).getId() == 8467876 && players.get(1).getId() == 8467875
 					|| players.get(0).getId() == 8467875 && players.get(1).getId() == 8467876) {
-				return "Sedinery!";
+				return ":boy: Sedinery! :boy:";
 			}
 		}
 
