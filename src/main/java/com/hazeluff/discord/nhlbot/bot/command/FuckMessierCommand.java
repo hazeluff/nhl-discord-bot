d995c0fc894c754f266f3c3bb4862eb68fc12a25
hazeluff <eugene@hazeluff.com>
2018-10-13 20:55:00 -0700

    Fuck matheson

--- src/main/java/com/hazeluff/discord/nhlbot/bot/command/FuckMessierCommand.java
+++ src/main/java/com/hazeluff/discord/nhlbot/bot/command/FuckMessierCommand.java
@@ -14,7 +14,9 @@ import sx.blah.discord.handle.obj.IMessage;
  */
 public class FuckMessierCommand extends Command {
 
-	private final static List<String> replies = Arrays.asList("FUCK MATHESON", "Fact: Matheson has no weiner.",
+	private final static List<String> replies = Arrays.asList(
+			"FUCK MATHESON", 
+			"Fact: Matheson has no weiner.",
 			"Matheson is ugly and his mom doesnt like him",
 			"RULES: \n" + "1. Respect Others\r\n"
 					+ "• No hate messenges about any hockey player and team except for Messier; "
@@ -22,7 +24,19 @@ public class FuckMessierCommand extends Command {
 			"Matheson: \"I did it because I'm irrelavent and stupid.\"",
 			"I bet Matheson is the type of dude who doesn't hold doors for people",
 			"༼ つ ◕\\_◕ ༽つPETEY TAKE MY HEAD ༼ つ ◕\\_◕ ༽つ",
-			"Matheson puts the box of cookies back into the cupboard when he finishes them");
+			"Matheson puts the box of cookies back into the cupboard when he finishes them.",
+			"I bet Matheson is the type of dude to put his bread in the fridge.",
+			"Matheson has a PhD in illegal hockey plays",
+			"Matheson is the type of guy who licks peanut butter off his fingers",
+			":train2:  spam this train :train2: to save Elias brain :train2:", 
+			"Matheson who?",
+			"Matheson is literally Hitler", 
+			"Confirmed: Matheson == Messier",
+			"Matheson? Meth(head brutally injuring innocent petters)son.",
+			"Matheson: \"Harming young people gives me sexual pleasure.\"",
+			"Mike Matheson\n" + 
+			"Florida Panthers #19. ~~Proud~~ _Disgraceful_ Boston College alum",
+			"To be fair, Petey didn’t make Matheson look like a butch. He was just born that way.");
 
 	public FuckMessierCommand(NHLBot nhlBot) {
 		super(nhlBot);
